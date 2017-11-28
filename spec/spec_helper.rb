f723b74b35983cbcb2d5e934db9b17dbfe8fee89
Tabish Raza <tabishraza@gmail.com>
2017-11-28 09:22:42 +0000

    first push

---
+++ spec/spec_helper.rb
@@ -0,0 +1,49 @@
+ENV["RACK_ENV"] = 'test'
+
+require 'simplecov'
+require 'simplecov-console'
+require 'database_cleaner'
+require 'capybara/rspec'
+require './app/app.rb'
+require './app/models/peep.rb'
+require 'orderly'
+require './spec/features/web_helper'
+require 'sinatra/flash'
+Capybara.app = Chitter
+
+
+
+
+RSpec.configure do |config|
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+end
